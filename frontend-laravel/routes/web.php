e563dc894702d1f9bca6a481f86851afe35650d0
Aebil Taskari Rusydi <aebiltaskari@gmail.com>
2023-11-18 13:43:32 +0700

    git push

---
+++ frontend-laravel/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('pages/index');
+});
+
+Route::get('/Login', function () { return view('pages/login');});
+Route::get('/Register', function () { return view('pages/register');});
+Route::get('/Chart', function () { return view('pages/chart');});

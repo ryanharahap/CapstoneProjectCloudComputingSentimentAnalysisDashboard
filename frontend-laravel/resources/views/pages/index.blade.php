154e881aef99bb658721aab72418bfd8dacb0a34
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-11-19 20:46:23 +0700

    chore: change title name and fix href in login and register

--- frontend-laravel/resources/views/pages/index.blade.php
+++ frontend-laravel/resources/views/pages/index.blade.php
@@ -15,7 +15,7 @@
         <nav class="navbar bg-light navbar-light">
             <a href="/
             " class="navbar-brand mx-4 mb-3">
-                <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>SentDash</h3>
+                <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>SENTDASH</h3>
             </a>
             <div class="d-flex align-items-center ms-4 mb-4">
                 <div class="position-relative">
--- frontend-laravel/resources/views/pages/register.blade.php
+++ frontend-laravel/resources/views/pages/register.blade.php
@@ -16,8 +16,8 @@
             <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                 <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
                     <div class="d-flex align-items-center justify-content-between mb-3">
-                        <a href="index.html" class="">
-                            <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>DASHMIN</h3>
+                        <a href="/" class="">
+                            <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>SENTDASH</h3>
                         </a>
                         <h3>Sign Up</h3>
                     </div>

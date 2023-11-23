ccbe96d808e89e8475f57c6b9bb475918d7d7da8
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-11-23 17:29:25 +0700

    chore: add redirect in login and register

--- frontend-laravel/resources/views/pages/login.blade.php
+++ frontend-laravel/resources/views/pages/login.blade.php
@@ -37,7 +37,7 @@
                         <a href="">Forgot Password</a>
                     </div>
                     <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                    <p class="text-center mb-0">Don't have an Account? <a href="">Sign Up</a></p>
+                    <p class="text-center mb-0">Don't have an Account? <a href="/Register">Sign Up</a></p>
                 </div>
             </div>
         </div>

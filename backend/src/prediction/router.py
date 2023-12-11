f0b6a379b30b18a17110bc202018b7b3269ed8d0
Jason Kanggara <jk.jasonkanggara@gmail.com>
2023-12-11 20:17:23 +0700

    chore: update predictions request and response model

--- backend/src/prediction/router.py
+++ backend/src/prediction/router.py
@@ -1,6 +1,7 @@
+from typing import List
 from fastapi import APIRouter
 from .services import PredictionService
-from .schemas import InputList, InputBase
+from .schemas import YoutubeRequest, PlaystoreRequest, NewsRequest
 
 router = APIRouter(
   prefix="/predict",
@@ -8,13 +9,13 @@ router = APIRouter(
 )
 
 @router.post("/playstore")
-async def playstore_prediction(data: InputBase):
+async def playstore_prediction(data: List[PlaystoreRequest]):
   return PredictionService().playstore_predict(data)
 
 @router.post("/youtube")
-async def youtube_prediction(data: InputBase):
+async def youtube_prediction(data: List[YoutubeRequest]):
   return PredictionService().youtube_predict(data)
 
 @router.post("/news")
-async def news_prediction(data: InputBase):
+async def news_prediction(data: List[NewsRequest]):
   return PredictionService().news_predict(data)
